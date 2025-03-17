76d1c79593934a11e2d04bde4b3a5a7188d38862
Panic <dburton900@gmail.com>
2025-03-17 02:34:48 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -10,9 +10,6 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-vim.opt.rtp:append("~/.local/share/nvim/lualib") -- Add custom Lua module path
-package.cpath = package.cpath .. ";~/.local/share/nvim/lualib/?.so" -- Add to cpath
-
 -- Set global indentation to 2 spaces
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
@@ -22,9 +19,9 @@ vim.opt.smarttab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.cindent = false
-
--- Set clipboard to use system clipboard
-vim.opt.clipboard = "unnamedplus"
+vim.o.scrolloff = 10
+vim.o.background = 'dark'
+vim.opt.number = true
 
 -- Filetype-specific indentation for TypeScript, JavaScript, TSX, and JSON
 vim.api.nvim_create_autocmd("FileType", {
@@ -44,34 +41,84 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     config = function()
-      require("telescope").setup {
-        defaults = {
-          -- file_ignore_patterns = { "%.git/", "node_modules/" }, -- Commented out to test
-          cache_picker = {
-            enable = true,
-            num_pickers = 10,
-          },
-        },
-      }
+      require("telescope").setup {}
     end,
   },
   {
-    "nvim-telescope/telescope-ui-select.nvim",
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
     config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-              cwd = vim.fn.getcwd(), -- Explicitly set CWD
-            },
-          },
-        },
-      }
-      require("telescope").load_extension("ui-select")
+      require('nvim-autopairs').setup({})
     end,
   },
-  -- Custom vim.ui.select override for CopilotChat #file:
-  -- Note: Place this after telescope-ui-select to ensure proper override
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+  
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+  },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
@@ -100,33 +147,87 @@ require("lazy").setup({
       end
     end,
   },
+  "nvim-tree/nvim-web-devicons",
   "neovim/nvim-lspconfig",
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer", -- Optional: for buffer completions
   "hrsh7th/cmp-path", -- Optional: for path completions
   "tpope/vim-fugitive",
-  "preservim/nerdtree",
-  "nvim-lualine/lualine.nvim",
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 35,
+          side = "left",
+        },
+        filters = {
+          dotfiles = false, -- Show dotfiles (optional tweak from earlier)
+        },
+        git = {
+          ignore = false, -- Show git-ignored files (from earlier)
+        },
+        renderer = {
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+            glyphs = { -- Optional: Customize icons if needed
+              default = "",
+              symlink = "",
+              folder = {
+                default = "",
+                open = "",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require('lualine').setup {
+        options = { theme = 'auto' },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        }
+      }
+    end
+  },
   {
     "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
     config = function()
       require("copilot").setup({
-        suggestion = { enabled = true },
         panel = {
           enabled = true,
+          auto_refresh = false,
           keymap = {
-            accept = "<C-y>",
-            next = "<C-n>",
-            prev = "<C-p>",
-            dismiss = "<C-e>",
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
           },
           layout = {
-            position = "right",
+            position = "bottom", -- | top | left | right | horizontal | vertical
             ratio = 0.4,
           },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
         },
         filetypes = {
           typescript = true,
@@ -138,101 +239,95 @@ require("lazy").setup({
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
-      "zbirenbaum/copilot.lua",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
+      { "zbirenbaum/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
-    config = function()
-      local ok, copilot_chat = pcall(require, "CopilotChat")
-      if not ok then
-        vim.notify("CopilotChat not found: " .. copilot_chat, vim.log.levels.ERROR)
-        return
-      end
-      local tiktoken_ok, tiktoken = pcall(require, "tiktoken_core")
-      if tiktoken_ok then
-        vim.notify("tiktoken_core loaded successfully", vim.log.levels.INFO)
-      else
-        vim.notify("tiktoken_core not found, using fallback token counting", vim.log.levels.WARN)
-      end
-      copilot_chat.setup {
-        context = "workspace",
-        mappings = {
-          submit_prompt = "<Enter>",
-          clear_chat = "<C-c>",
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      model = 'gpt-4o',
+      prompts = {
+        file = {
+          prompt = '#file:',
+          picker = function()
+            local cwd = vim.fn.getcwd()
+            local telescope = require("telescope.builtin")
+            telescope.find_files({
+              cwd = cwd,
+              hidden = true,
+              -- file_ignore_patterns = { "%.git/", "node_modules/" },
+            })
+          end,
         },
-      }
+      },
+    },
+    config = function(_, opts)
+      require("CopilotChat").setup(opts)
     end,
   },
   "HiPhish/rainbow-delimiters.nvim",
+  "sbdchd/neoformat",
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
     config = function()
       vim.cmd.colorscheme "catppuccin"
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
     end,
   },
-  "sbdchd/neoformat",
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  }
 })
 
 -- nvim-cmp setup
 local cmp = require("cmp")
 cmp.setup {
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+  },
   mapping = {
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ["<C-Space>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn.getline('.'):match('#file:') then
-        fallback() -- Allow CopilotChat to handle #file: without completion
-      elseif vim.fn.getline('.') == '' then
-        vim.api.nvim_put({string.rep(' ', vim.bo.shiftwidth)}, 'c', true, true) -- Insert shiftwidth spaces on empty line
-      elseif vim.fn.getline('.'):sub(1, vim.fn.col('.') - 1) == '' then
-        vim.api.nvim_put({string.rep(' ', vim.bo.shiftwidth)}, 'c', true, true) -- Insert shiftwidth spaces if no chars before cursor
       else
-        cmp.complete() -- Trigger completion if chars exist before cursor
+        cmp.complete()
       end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<C-S-Up>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       else
         fallback()
       end
-    end, { 'i', 's' }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept completion with Enter
-    ['<C-e>'] = cmp.mapping.abort(), -- Close completion menu
-    ['<C-Space>'] = cmp.mapping.complete(), -- Manually trigger completion
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
+    end, { "i", "s" }),
+    ["<C-S-Down>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<CR>"] = cmp.mapping.confirm({
+      select = true,
+      behavior = cmp.ConfirmBehavior.Insert,
+    }),
+    ["<C-e>"] = cmp.mapping.abort(),
   },
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
-}
-
-require("nvim-treesitter.configs").setup {
-  ensure_installed = {"c", "lua", "vim", "python", "javascript", "typescript", "tsx", "json"},
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<C-space>",
-      node_incremental = "<C-space>",
-      node_decremental = "<S-space>",
-    },
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = 1000,
+  experimental = {
+    ghost_text = false,
   },
 }
 
@@ -251,49 +346,48 @@ lspconfig.ts_ls.setup {
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
   end,
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
 }
 
 -- Neoformat configuration
-vim.g.neoformat_typescript_prettier = {
-  exe = "prettier",
-  args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
-  stdin = 1,
-}
-vim.g.neoformat_javascript_prettier = {
-  exe = "prettier",
-  args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
-  stdin = 1,
-}
-vim.g.neoformat_typescriptreact_prettier = {
-  exe = "prettier",
-  args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
-  stdin = 1,
-}
-vim.g.neoformat_json_prettier = {
-  exe = "prettier",
-  args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
-  stdin = 1,
-}
-vim.g.neoformat_enabled_typescript = {"prettier"}
-vim.g.neoformat_enabled_javascript = {"prettier"}
-vim.g.neoformat_enabled_typescriptreact = {"prettier"}
-vim.g.neoformat_enabled_json = {"prettier"}
-
--- Leader keybinding to format the entire file (Prettier + reindent)
-vim.keymap.set("n", "<leader>f", function()
-  vim.cmd("Neoformat")
-  vim.cmd("normal! gg=G")
-  vim.cmd("write")
-end, { desc = "Format File (Prettier + Reindent)" })
-
--- Auto-format on save for specific filetypes
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.ts", "*.tsx", "*.js", "*.json" },
-  callback = function()
-    vim.cmd("Neoformat")
-  end,
-})
+-- vim.g.neoformat_typescript_prettier = {
+--   exe = "prettier",
+--   args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
+--   stdin = 1,
+-- }
+-- vim.g.neoformat_javascript_prettier = {
+--   exe = "prettier",
+--   args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
+--   stdin = 1,
+-- }
+-- vim.g.neoformat_typescriptreact_prettier = {
+--   exe = "prettier",
+--   args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
+--   stdin = 1,
+-- }
+-- vim.g.neoformat_json_prettier = {
+--   exe = "prettier",
+--   args = {"--stdin-filepath", vim.fn.expand("%:p"), "--single-quote", "--trailing-comma", "es5"},
+--   stdin = 1,
+-- }
+-- vim.g.neoformat_enabled_typescript = {"prettier"}
+-- vim.g.neoformat_enabled_javascript = {"prettier"}
+-- vim.g.neoformat_enabled_typescriptreact = {"prettier"}
+-- vim.g.neoformat_enabled_json = {"prettier"}
 
 -- Telescope keybindings
 local telescope = require("telescope.builtin")
@@ -322,17 +416,57 @@ vim.keymap.set("n", "<C-p>", function()
 end, { desc = "Telescope: Find Files" })
 vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Telescope: Buffers" })
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope: Live Grep" })
+vim.keymap.set("n", "<leader>t", ":below 10split | terminal<CR>", { desc = "Open terminal below" })
 
--- Window navigation keybindings
-local function set_window_keymap(lhs, rhs, desc)
-  if rhs == nil then
-    vim.notify("Invalid rhs for keymap: " .. lhs, vim.log.levels.ERROR)
-    return
+-- Copilot
+vim.keymap.set("i", "<C-\\>", function()
+  require("copilot.suggestion").toggle_auto_trigger()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").dismiss()
+  else
+    require("copilot.suggestion").next() -- Trigger the next suggestion
   end
-  vim.keymap.set("n", lhs, rhs, { desc = desc })
-end
+end, { desc = "Toggle/Trigger Copilot suggestion" })
 
-set_window_keymap("<C-h>", "<C-w>h", "Move to Left Window")
-set_window_keymap("<C-j>", "<C-w>j", "Move to Window Below")
-set_window_keymap("<C-k>", "<C-w>k", "Move to Window Above")
-set_window_keymap("<C-l>", "<C-w>l", "Move to Right Window")
+-- lsp
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    wrap = true,
+    max_width = 80,
+    border = "rounded",
+  }
+)
+
+-- Enable wrapping for diagnostic popups
+vim.diagnostic.config({
+  float = {
+    wrap = true,
+    max_width = 80,
+    border = "rounded",
+    source = "always",
+  },
+})
+
+-- Keybinding to show diagnostics in a floating window
+vim.keymap.set("n", "<leader>dd", function()
+  vim.diagnostic.open_float(nil, {
+    scope = "line", -- Show diagnostics for the current line
+    border = "rounded", -- Optional: Add a border for better visibility
+    max_width = 80, -- Ensure the window doesn't run off the screen
+    max_height = 20, -- Limit the height of the window
+    focusable = true, -- Allow scrolling/interaction
+    source = "always", -- Show the source of the diagnostic (e.g., ts_ls)
+  })
+end, { desc = "toggles local troubleshoot" })
+
+vim.g.copilot_enabled = 0
+vim.cmd("Copilot disable")
+
+-- New tab
+vim.keymap.set("n", "te", ":tabedit")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>")
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>")
+
+-- Split Window
+vim.keymap.set("n", "ss", ":split<Return>")
+vim.keymap.set("n", "sv", ":vsplit<Return>")
